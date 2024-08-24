79f492f7451e65d6c57a48b873da8ac32376ed84
indasashko123 <indasashko123@mail.ru>
2024-08-24 16:32:51 +0300

    Update Bar.ts

--- application/queries/Bar.ts
+++ application/queries/Bar.ts
@@ -3,7 +3,10 @@ import { BaseManyQuery } from "./BaseManyQuery";
 import { BaseOneQuery } from "./BaseOneQuerry";
 
 export class BarManyQuery extends BaseManyQuery<BarTarget> {
-
+    date? : {
+        dateFrom : Date;
+        dateTo : Date;
+    }
 }
 
 export class BarOneQuery extends BaseOneQuery<BarTarget> {
